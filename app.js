19c363a7f85bfb0687ae445b61630d97eeb57caa
Matt <price.mc1x@gmail.com>
2018-08-15 15:52:03 -0600

    Console log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ async function run() {
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
+        console.log("Connected to DB.");
         var ProxySchema = new Schema({
             url: String,
             port: String,
