73687fdd004e27005f0fbacf08db4f8af9d14055
Matt <price.mc1x@gmail.com>
2018-08-15 15:50:56 -0600

    Change Streams Update

--- app.js
+++ app.js
@@ -1,4 +1,39 @@
 var proxy = require('redbird')({port: 80});
 const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
-proxy.register("dev.mattcprice.com", "http://localhost:81");
+
+async function run() {
+    mongoose.connect('mongodb://localhost/proxy');
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function() {
+        var ProxySchema = new Schema({
+            url: String,
+            port: String,
+            created_at: Date
+        });
+        ProxySchema.methods.add = function() {
+            proxy.register(this.url + ".mattcprice.com", "http://localhost:" + this.port);
+        }
+        ProxySchema.methods.remove = function() {
+            proxy.unregister(url + ".mattcprice.com");
+        }
+
+        var ProxyObject = mongoose.model('ProxyObject', ProxySchema);
+
+        ProxyObject.find(function (err, proxies) {
+            if (err) return console.error(err);
+            array.forEach(async element => {
+                element.add();
+            });
+        })
+
+        ProxyObject.watch().
+            on('change', change => console.log(change));
+    });
+
+
+
+};
